f49b401a44946ba160240bd691a056b2ee1f957c
JoKeR0313 <istvanfilimon@gmail.com>
2017-04-25 16:07:45 +0200

    User class implemented!

---
+++ src/main/java/com/codecool/shop/model/User.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by joker on 2017.04.25..
+ */
+public class User {
+    private List<Order> orders= new ArrayList<>();
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String billingAddress;
+    private String shippingAddress;
+
+    User(String name, String email, String phoneNumber, String billingAddress, String shippingAddress){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.billingAddress = billingAddress;
+        this.shippingAddress = shippingAddress;
+    }
+
+    public void setOrders(Order order){
+        orders.add(order);
+    }
+}
