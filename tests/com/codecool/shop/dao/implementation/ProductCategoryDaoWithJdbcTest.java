d7c977a2629047994b05b83ff010479875e35b7e
Kata Meszaros <kata.meszaros92@gmail.com>
2017-05-10 10:35:10 +0200

    Added empty testfiles for product, productCategory and supplier daos

---
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoWithJdbcTest.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by kata on 2017.05.10..
+ */
+class ProductCategoryDaoWithJdbcTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void find1() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+}
---
+++ tests/com/codecool/shop/dao/implementation/ProductDaoWithJdbcTest.java
@@ -0,0 +1,39 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by kata on 2017.05.10..
+ */
+class ProductDaoWithJdbcTest {
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void find1() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void getBy1() {
+    }
+
+}
