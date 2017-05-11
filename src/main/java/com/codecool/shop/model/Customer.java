1fc7bf166f9bbef1a72ae1d6fce414a3faef970e
Emese Billing <talebilling@gmail.com>
2017-05-11 16:24:17 +0200

    feature/talebillig_customer branch's latest commit is cherry-picked and merged.

--- src/main/java/com/codecool/shop/model/Customer.java
+++ src/main/java/com/codecool/shop/model/Customer.java
@@ -1,12 +1,55 @@
 package com.codecool.shop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by joker on 2017.05.11..
+ * Created by joker on 2017.04.25..
  */
-public class Customer {
-    private String name;
-    private String email;
-    private Integer phoneNumber;
+public class Customer extends Person {
+    private List<Order> orders = new ArrayList<>();
+    private String phoneNumber;
     private String billingAddress;
     private String shippingAddress;
+
+    Customer(String name, String email, String phoneNumber, String billingAddress, String shippingAddress) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.billingAddress = billingAddress;
+        this.shippingAddress = shippingAddress;
+    }
+
+    public static Customer createUser(String name, String email, String phoneNumber, String billingAddress, String shippingAddress) {
+        Customer newCustomer = new Customer(name, email, phoneNumber, billingAddress, shippingAddress);
+        return newCustomer;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setOrders(Order order) {
+        orders.add(order);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", orders=" + orders +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", billingAddress='" + billingAddress + '\'' +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                '}';
+    }
 }
